4456deef461d4f7f5c68ea88f6b6d23e6f1073b0
jasem66 <jasemsarafraz66@gmail.com>
2022-08-16 14:48:23 +0300

    Update filter_reducer.js

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,14 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice=action.payload.map((p)=>p.price)
+    maxPrice= Math.max(...maxPrice)
+
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters:{...state.filters,max_price:maxPrice,price:maxPrice}
     }
   }
 
@@ -38,7 +42,7 @@ const filter_reducer = (state, action) => {
     let tempProducts = [...filtered_products]
     if (sort === 'price-lowest') {
       tempProducts = tempProducts.sort((a, b) => {
-        if(a.price > b.price){
+        if(a.price < b.price){
           return -1
         }
         if(a.price > b.price){
@@ -60,6 +64,13 @@ const filter_reducer = (state, action) => {
       })
       }
     return { ...state,filtered_products : tempProducts }
+  }
+  if(action.type===UPDATE_FILTERS){
+    const {name,value} = action.payload
+    return{...state,filters:{...state.filters,[name]:value}}
+  }
+  if(action.type===FILTER_PRODUCTS){
+    return{...state}
   }
 
   throw new Error(`No Matching "${action.type}" - action type`)
